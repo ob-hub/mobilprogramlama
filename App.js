3189bdf8d34cb513832d27804458354c690f51e1
Erhan BİÇER <iletisim@erhanbicer.com>
2020-11-24 19:42:12 +0300

    Projede react-navigation ile ekran geçişleri yapıldı

--- App.js
+++ App.js
@@ -1,161 +1,43 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { Button, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({ navigation, ...props }) {
+   return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <Text>Merhaba Mobil</Text>
+         <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+      </View>
+   );
+}
+
+function DetailsScreen() {
+   return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <Text>Details Screen</Text>
+      </View>
+   );
+}
+
+const Stack = createStackNavigator();
 
 const App = (props) => {
    return (
-      <ScrollView style={{ backgroundColor: 'gray' }}>
-         {data.map((item, index) => {
-            return (
-               <Text
-                  style={{
-                     height: 50
-                  }}
-                  key={index}>
-                  {item.first_name}
-               </Text>
-            );
-         })}
-      </ScrollView>
+      <NavigationContainer>
+         <Stack.Navigator>
+            <Stack.Screen
+               options={{
+                  headerTitle: 'Anasayfa'
+               }}
+               name="Home"
+               component={HomeScreen}
+            />
+            <Stack.Screen name={'Details'} component={DetailsScreen} />
+         </Stack.Navigator>
+      </NavigationContainer>
    );
 };
 
 export default App;
-
-const data = [
-   {
-      first_name: 'Fionna',
-      last_name: 'Monteaux'
-   },
-   {
-      first_name: 'Jesselyn',
-      last_name: 'Barwis'
-   },
-   {
-      first_name: 'Vinny',
-      last_name: 'Arnaldo'
-   },
-   {
-      first_name: 'Tandy',
-      last_name: 'Geaves'
-   },
-   {
-      first_name: 'Ramsey',
-      last_name: 'Welband'
-   },
-   {
-      first_name: 'Rachael',
-      last_name: 'Flamank'
-   },
-   {
-      first_name: 'Rodney',
-      last_name: 'Jewell'
-   },
-   {
-      first_name: 'Parry',
-      last_name: 'Benoit'
-   },
-   {
-      first_name: 'Tuck',
-      last_name: 'Cannop'
-   },
-   {
-      first_name: 'Olympia',
-      last_name: 'McCrillis'
-   },
-   {
-      first_name: 'Marietta',
-      last_name: 'Pevsner'
-   },
-   {
-      first_name: 'Craggy',
-      last_name: 'Jirus'
-   },
-   {
-      first_name: 'Tracy',
-      last_name: 'Grogan'
-   },
-   {
-      first_name: 'Roderigo',
-      last_name: 'Leggan'
-   },
-   {
-      first_name: 'Ashlin',
-      last_name: 'Sellwood'
-   },
-   {
-      first_name: 'Lezlie',
-      last_name: 'Droogan'
-   },
-   {
-      first_name: 'Etty',
-      last_name: 'Shayler'
-   },
-   {
-      first_name: 'Gib',
-      last_name: 'Offell'
-   },
-   {
-      first_name: 'Annalee',
-      last_name: 'Fenty'
-   },
-   {
-      first_name: 'Cookie',
-      last_name: 'Tooker'
-   },
-   {
-      first_name: 'Rodney',
-      last_name: 'Jewell'
-   },
-   {
-      first_name: 'Parry',
-      last_name: 'Benoit'
-   },
-   {
-      first_name: 'Tuck',
-      last_name: 'Cannop'
-   },
-   {
-      first_name: 'Olympia',
-      last_name: 'McCrillis'
-   },
-   {
-      first_name: 'Marietta',
-      last_name: 'Pevsner'
-   },
-   {
-      first_name: 'Craggy',
-      last_name: 'Jirus'
-   },
-   {
-      first_name: 'Tracy',
-      last_name: 'Grogan'
-   },
-   {
-      first_name: 'Roderigo',
-      last_name: 'Leggan'
-   },
-   {
-      first_name: 'Ashlin',
-      last_name: 'Sellwood'
-   },
-   {
-      first_name: 'Lezlie',
-      last_name: 'Droogan'
-   },
-   {
-      first_name: 'Etty',
-      last_name: 'Shayler'
-   },
-   {
-      first_name: 'Gib',
-      last_name: 'Offell'
-   },
-   {
-      first_name: 'Annalee',
-      last_name: 'Fenty'
-   },
-   {
-      first_name: 'Cookie',
-      last_name: 'Tooker'
-   }
-];
--- index.js
+++ index.js
@@ -4,8 +4,6 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-import FlatlistView from './FlatlistView';
-import FlatlistViewClass from './FlatlistViewClass';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => FlatlistViewClass);
+AppRegistry.registerComponent(appName, () => App);
