b66a3f567fea225ea495b5d754b1658aea613319
Erhan BİÇER <iletisim@erhanbicer.com>
2020-12-08 19:29:08 +0300

    Router yönlendirilmesi yapıldı

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import Router from './router/Router';
+
+const App = (props) => {
+   return (
+     <Router />
+   );
+};
+
+export default App;
