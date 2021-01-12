7de61f73e9d01fa3aa12da72e5b07d1d7a8c5036
Erhan BİÇER <iletisim@erhanbicer.com>
2021-01-12 19:21:11 +0300

    İkonlar eklendi

---
+++ src/resources/icons/ChatsIcon.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+function ChatsIcon({ color = 'black', width = 18, height = 18, ...props }) {
+   return (
+      <Svg xmlns="http://www.w3.org/2000/svg" fill={color} viewBox="0 0 24 24" width={width} height={height} {...props}>
+         <Path d="M0 0h24v24H0V0z" fill="none" />
+         <Path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM7 9h10c.55 0 1 .45 1 1s-.45 1-1 1H7c-.55 0-1-.45-1-1s.45-1 1-1zm6 5H7c-.55 0-1-.45-1-1s.45-1 1-1h6c.55 0 1 .45 1 1s-.45 1-1 1zm4-6H7c-.55 0-1-.45-1-1s.45-1 1-1h10c.55 0 1 .45 1 1s-.45 1-1 1z" />
+      </Svg>
+   );
+}
+
+export default ChatsIcon;
